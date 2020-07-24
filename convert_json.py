e2354d4367b365389c36e9b76423823ac8920c21
bondgeodima <bondgeodima@gmail.com>
2020-07-24 12:39:31 +0300

    nomeroff

---
+++ convert_json.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+dirname_in = 'D:/TEMP/_____NIGD/mirbank/_in/upload/out_json/'
+dirname_out = 'D:/TEMP/_____NIGD/mirbank/_in/upload/out_json_convert/'
+
+for filename in os.listdir(dirname_in):
+    with open(dirname_in + filename) as json_file_in:
+        data = json.load(json_file_in)
+        with open(dirname_out + filename, 'w') as json_file_out:
+            print (json_file_out)
+            json.dump(data['data']['geojson'], json_file_out)
