c2d6f3e5e3fe147436264d00b13165aedb238df9
bondgeodima <bondgeodima@gmail.com>
2020-07-24 13:26:19 +0300

    nomeroff

--- convert_json.py
+++ convert_json.py
@@ -8,5 +8,5 @@ for filename in os.listdir(dirname_in):
     with open(dirname_in + filename) as json_file_in:
         data = json.load(json_file_in)
         with open(dirname_out + filename, 'w') as json_file_out:
-            print (json_file_out)
+            print (filename)
             json.dump(data['data']['geojson'], json_file_out)
